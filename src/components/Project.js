caeef4a2b1c8a563abc6453cf615a85fb580124c
Michael Cook <mcook0775@gmail.com>
2020-09-18 20:33:10 -0400

    Changes to Project view

--- src/components/Project.js
+++ src/components/Project.js
@@ -20,25 +20,24 @@ export default function Project() {
 
   return (
     <Content>
-      
-      {/* Left column */}
-      <div className="row">
-        
-        <div className="col-8">
+      <div className="d-flex justify-content-between">
+        <div>
           <h1>{project.title}</h1>
           <h5>Technologies:</h5>
           <Tags tags={project.technologies} />
-          <img className="img-fluid mt-3 mb-2 shadow" src={project.img_url} />
-          <h4>Description</h4>
-          <Post input={project.description} />
         </div>
 
-        {/* Right column */}
-        <div className="col mt-5 ml-3">
+        <div className="col-5 mt-5 ml-3">
           <a className="btn btn-block btn-success" href={project.repo_url} target="_blank">View Repo</a>
           <a className="btn btn-block btn-primary" href={project.live_link} target="_blank">View Live Demo</a>
         </div>
       </div>
+      
+      <img className="img-fluid mt-3 mb-2 shadow" src={project.img_url} />
+      <h4>Description</h4>
+      <Post input={project.description} />
+        
+      
     </Content>
   )
 }
