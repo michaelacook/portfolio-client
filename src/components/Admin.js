60fb41af133a7e1a2169f10f49d2d1b5a82c4bad
Michael A. Cook <mcook0775@gmail.com>
2020-09-25 18:57:10 -0400

    alteration to admin view

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -26,37 +26,19 @@ export default function Admin() {
     <Content>
       <h1>Admin Dashboard</h1>
 
-      <div className="row">
+      <div className="row mt-5">
         <div className="col-4 pb-3 border rounded mr-4">
           <h5 className="mt-2">Manage Posts</h5>
-          <Link to="/posts/new" className="btn btn-lg btn-outline-success">
+          <Link to="/posts/new" className="btn btn-lg btn-link">
             New Post
           </Link>
         </div>
 
         <div className="col-4 pb-3 border rounded">
           <h5 className="mt-2">Manage Projects</h5>
-          <Link to="/projects/new" className="btn btn-lg btn-outline-success">
+          <Link to="/projects/new" className="btn btn-lg btn-link">
             New Project
           </Link>
-          <label htmlFor="">Delete a project</label>
-          <select
-            onChange={(e) => setProjectToDelete(e.target.value)}
-            class="form-control"
-          >
-            <option>Select a project</option>
-            {projects.map((project) => (
-              <option key={project.id} value={project.id}>
-                {project.title}
-              </option>
-            ))}
-          </select>
-          <button
-            onClick={deleteProject}
-            className="btn btn-sm btn-block btn-primary"
-          >
-            Go
-          </button>
         </div>
       </div>
     </Content>
