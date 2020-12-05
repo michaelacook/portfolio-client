231965e56b8b879132938b34e48508f20c2c6444
Michael Cook <mcook0775@gmail.com>
2020-12-05 10:37:56 -0500

    Add right position menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,6 +13,13 @@ export default function Navbar({ user }) {
           </Menu.Item>
           <Menu.Item as={NavLink} to="/about" name="About" />
           <Menu.Item as={NavLink} to="/blog" name="Blog" />
+          {user ? (
+            <Menu.Menu position="right">
+              <Menu.Item as={NavLink} to="/posts/new" name="New Post" />
+              <Menu.Item as={NavLink} to="/admin" name="Admin" />
+              <Menu.Item as={NavLink} to="/signout" name="Signout" />
+            </Menu.Menu>
+          ) : null}
         </Container>
       </Menu>
     </Segment>
